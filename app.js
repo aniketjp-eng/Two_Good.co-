3015306a6e6425a6803e43c432cc896132c2464b
Aniket Prajapati <aniketjp.eng@gmail.com>
2026-01-13 22:23:59 +0530

    added images in page 3

--- app.js
+++ app.js
@@ -45,3 +45,10 @@ gsap.from("#page1 #video-container", { // Any GSAP animation written directly (n
     duration:0.3
 })
 loadingCharAnimation();
+
+document.addEventListener("mousemove", function(dets) {
+    gsap.to("#cursor", {
+        left:dets.x,
+        top:dets.y
+    })
+})
